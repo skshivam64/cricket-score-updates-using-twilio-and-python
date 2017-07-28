3836ce4d59489606fe43a52b25e877e611c8f2a2
Shivam Kumar <engineeraccidental@gmail.com>
2017-07-28 13:24:56 +0530

    Added a prettify function
    prettify function makes the message to be sent look prettier by adding spaces and newlines at right places

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ def extract_score(my_url, *id_kargs):
         resp = request.urlopen(my_url).read()
         soup = BeautifulSoup(resp, "html.parser")
         score = soup.find_all("a", class_=id_kargs[0])
-        return score[:4]
-        # extracted just the four top results
-        # one can also go with return score[0] if he/she wants just the best result
+        return score
     except Exception as e:
         print(str(e))
         return "Network Error"
@@ -38,13 +36,37 @@ def send_sms(acc_sid, auth_tok, to_num, from_num, score):
     except Exception as e:
         print(str(e))
 
+        
+def prettify(data):
+    str = ''
+    for each in data:
+        for each_next in each:
+            try:
+                str += each_next.get_text()
+            except Exception as e:
+                str += each_next
+            str += ' '
+        str += '\n'
+    return str
+
 
 def main():
     while 1:
-        data = extract_score("http://www.cricbuzz.com", "cb-font-12")
-        for each in data:
-            send_sms(account_sid, auth_token, receiver, sender, each.get_text())
+        my_data = extract_score("http://www.cricbuzz.com", "cb-font-12")
+        sms_str = ''
+        my_data = my_data[:4]
+        # extracted just the four top results
+        # one can also go with my_data[0] if he/she wants just the topmost result
+
+        for each in my_data:
+            sms_str += prettify(each)
+            sms_str += "...............................................\n"
+
+        send_sms(account_sid, auth_token, receiver, sender, sms_str)
+        print(sms_str)
         time.sleep(600)
         # delayed further execution by 10 minutes
+        
+        
 if __name__ == "__main__": main()
 
